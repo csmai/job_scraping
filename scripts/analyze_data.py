a9084be08148a6212cf55fcbef832c7997e560f0
csmai <maicsilla@gmail.com>
2023-10-05 15:33:10 +0200

    data analyzation and result csv

--- scripts/analyze_data.py
+++ scripts/analyze_data.py
@@ -8,6 +8,17 @@ DB_URI = f"postgresql://postgres:{os.getenv('P4PASSWD')}@localhost:5432/prof_scr
 TABLE_NAMES = ["data_eng_prf", "data_eng_nof"]
 
 
+# Function to fetch data from the database
+def fetch_data_from_db():
+    engine = create_engine(DB_URI)
+    combined_data = pd.DataFrame()
+    for table_name in TABLE_NAMES:
+        query = f"SELECT * FROM {table_name};"
+        data_df = pd.read_sql_query(query, engine)
+        combined_data = pd.concat([combined_data, data_df], ignore_index=True)
+    return combined_data
+
+
 def analyze_and_visualize_tech_stack(dataframe):
     # 'job_tech_stack' is a column containing tech stack information in the DataFrame
     tech_stack = [
@@ -23,3 +34,14 @@ def analyze_and_visualize_tech_stack(dataframe):
     plt.ylabel("Frequency")
     plt.title("Tech Stack Frequency")
     plt.show()
+
+
+if __name__ == "__main__":
+    # Fetch data from the database
+    combined_data = fetch_data_from_db()
+
+    # Save the combined data to a CSV (for analysis outside this script)
+    combined_data.to_csv("combined_data.csv", index=False)
+
+    # Analyze the tech stack based on the fetched data
+    analyze_and_visualize_tech_stack(combined_data)
